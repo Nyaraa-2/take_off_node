832a48ce57b8ea9294ae6ebe142ccb513ab9fade
Nyaraa-2 <marina.vandenbosch@outlook.fr>
2022-03-16 14:12:13 +0100

    feat : add fichier config .env

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 import Pool from 'pg-pool'
+import 'dotenv/config'
 export const database = new Pool({
-  user: 'enzoavlt',
-  host: 'postgresql-enzoavlt.alwaysdata.net',
-  database: 'enzoavlt_takeoff',
-  password: 'takeoffbdd',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
