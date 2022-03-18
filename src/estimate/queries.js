9c193f1e5c8f5d80df49231cdf535854eb977d44
Enzo Averlant <enaverlant@gmail.com>
2022-03-18 14:21:19 +0100

    :bug: fix : bug post estimate fixed

--- src/estimate/queries.js
+++ src/estimate/queries.js
@@ -4,4 +4,4 @@
 export const GET_ESTIMATE_SQL = 'SELECT * FROM estimate'
 
 export const POST_ESTIMATE_SQL =
-  'INSERT INTO estimate(job_title, mission, from_when, duration, home_office, price, id_status, id_freelance, id_customer) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'
+  'INSERT INTO estimate(job_title, mission, from_when, duration, home_office, price, id_status, id_freelance, id_customer) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)'
