3cade9b378b3432c5045287d087b3ed560abf41c
Nyaraa-2 <marina.vandenbosch@outlook.fr>
2022-03-28 16:33:56 +0200

    :hammer: ajout d'un vrai test

--- tests/status/statusRepository.spec.js
+++ tests/status/statusRepository.spec.js
@@ -1,24 +1,30 @@
 import { getStatus } from '../../src/status/statusRepository.js'
 
-jest.mock('../../src/status/statusRepository.js', () => ({
-  getStatus: jest.fn(async () => {
-    return Promise.resolve([
-      { id: 1, name: 'Envoyé' },
-      { id: 2, name: 'En cours' },
-    ])
-  }),
+jest.mock('../../db.js', () => ({
+  database: {
+    query: jest.fn(async () => {
+      return Promise.resolve({
+        rows: [
+          { id: 1, name: 'Envoyé' },
+          { id: 2, name: 'En cours' },
+        ],
+      })
+    }),
+  },
 }))
 
 describe('Status Repository', () => {
   describe('GetStatus', () => {
     test('Should Return Status', async () => {
-      //* arrange
+      //* arrange : GET_STATUS_SQL
       const status = [
         { id: 1, name: 'Envoyé' },
         { id: 2, name: 'En cours' },
       ]
+
       //* act
       const data = await getStatus()
+
       //* assert
       expect(data).toStrictEqual(status)
     })
